6a70edd08491aaf633c84e59efcc798235d84c1a
Abdulkadir TÜRKAN <53895340+abdulkadirturkan@users.noreply.github.com>
2025-11-29 20:18:50 +0300

    Create NumberofCommonFactors.java

---
+++ Number of Common Factors/NumberofCommonFactors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int g=gcd(a,b);
+        int output = 0;
+        
+        for(int i = 1; i*i <=g; i++){
+            if(g%i == 0){
+                output++;
+                if(i != g/i) output++;
+            }
+        }
+        return output;
+    }
+    private int gcd(int x, int y){
+        while(y != 0){
+            int t = x % y;
+            x = y;
+            y = t;
+        }
+        return x;
+    }
+}
