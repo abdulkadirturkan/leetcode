326000cc5884e7f21e53c3596747dc4bf66ab805
Abdulkadir TÜRKAN <53895340+abdulkadirturkan@users.noreply.github.com>
2025-11-30 10:16:43 +0300

    Create MakeSumDivisiblebyP.java

---
+++ Make Sum Divisible by P/MakeSumDivisiblebyP.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for(int x : nums) total += x;
+
+        int rem = (int)(total % p);
+        if(rem == 0) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        long prefix = 0;
+        int result = nums.length;
+
+        for(int i = 0; i < nums.length; i++){
+            prefix = (prefix + nums[i]) % p;
+            int need = (int)((prefix - rem + p) % p);
+
+            if(map.containsKey(need)){
+                result = Math.min(result, i - map.get(need));
+            }
+            
+            map.put((int)prefix, i);
+        }
+        return result == nums.length ? -1 : result;
+    }
+}
