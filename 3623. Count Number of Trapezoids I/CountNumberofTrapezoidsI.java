19dc0a7c14e61094f0d9bbf711ca2d6985210293
Abdulkadir TÜRKAN <53895340+abdulkadirturkan@users.noreply.github.com>
2025-12-02 11:32:01 +0300

    Update CountNumberofTrapezoidsI.java

--- 3623. Count Number of Trapezoids I/CountNumberofTrapezoidsI.java
+++ 3623. Count Number of Trapezoids I/CountNumberofTrapezoidsI.java
@@ -7,24 +7,24 @@ class Solution {
         for(int[] p: points){
             countByY.put(p[1], countByY.getOrDefault(p[1], 0L) + 1);
         }
-        List<Long> counts = new ArrayList<>(countByY.values());
-        int m = counts.size();
 
-        long[] c2= new long[m];
-        for(int i = 0; i < m; i++){
-            long c = counts.get(i);
-            if(c >= 2) c2[i] = (c * (c - 1) / 2) % MOD;
-        }
+        long S = 0;
+        long S2 = 0;
 
-        long ans = 0;
 
-        for(int i = 0; i < m; i++){
-            if(c2[i] == 0) continue;
-            for(int j = i+1; j < m; j++){
-                if(c2[j] == 0) continue;
-                ans = (ans + (c2[i] * c2[j]) % MOD) % MOD;
+       for (long c : countByY.values()) {
+            if (c >= 2) {
+                long A = (c * (c - 1) / 2) % MOD;
+                S = (S + A) % MOD;
+                S2 = (S2 + (A * A) % MOD) % MOD;
             }
         }
+
+        long Ssq = (S * S) % MOD;
+        long diff = (Ssq - S2 + MOD) % MOD;
+
+        long inv2 = (MOD + 1) / 2;
+        long ans = (diff * inv2) % MOD;
 
         return (int) ans;
     }
